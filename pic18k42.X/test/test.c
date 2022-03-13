9c1ce8bfac90014ea892e15ef4cf23a717d29cf3
MJ <56290324+mjneri@users.noreply.github.com>
2022-03-13 20:42:37 +0800

    Update test.c
    added test code from k42-oled project

--- pic18k42.X/test/test.c
+++ pic18k42.X/test/test.c
@@ -41,10 +41,53 @@ static void TEST_OLED(void)
     OLED_Initialize();
     OLED_ClearDisplay();
     
-    displayMenu(0);
+    OLED_DrawBitmap();
+    __delay_ms(1000);
+    OLED_ClearDisplay();
+//    OLED_DrawString(0, 20, "Hello world!", font5x7, false);
+//    OLED_DrawString(9, 0, "The quick brown fox", font5x7, true);
+    
+    displayMenu(0xFF);
     
     while(1)
     {
+        // 2022-02-01 >> Simple test for the set pixel function
+//        for(uint8_t i = 0; i < SH1106_LCD_HEIGHT; i+=8)
+//        {
+//            for(uint8_t j = 0; j < 8; j++)
+//            {
+//                for(uint8_t k = 0; k < SH1106_SEGMENTS; k++)
+//                {
+//                    OLED_SetPixel(j + i, k, 1);
+//                }
+//            }
+//        }
+//        for(uint8_t i = 0; i < SH1106_LCD_HEIGHT; i+=8)
+//        {
+//            for(uint8_t j = 0; j < SH1106_SEGMENTS; j++)
+//            {
+//                for(uint8_t k = 0; k < 8; k++)
+//                {
+//                    OLED_SetPixel(k + i, j, 0);
+//                }
+//            }
+//        }
+//        for(uint8_t i = 0; i < 64; i++)
+//        {
+////            OLED_SetPixel(i, 5, 1);
+////            OLED_SetCursor(0, i);
+////            OLED_SendData((uint8_t []){0xFF}, 1);
+////            __delay_ms(100);
+//            OLED_SetCursor(i, 0);
+//            OLED_RecvData(dummyRead, sizeof(dummyRead));
+////            OLED_RecvData(&dummyRead, 1);
+////            OLED_RecvData(&dummyRead, 1);
+////            OLED_RecvData(&dummyRead, 1);
+////            OLED_RecvData(&dummyRead, 1);            
+//            __delay_ms(100);
+//        }
+        
+        // 2022-02-26 Test code
         if(switchSelected)
         {
             switchSelected = false;
