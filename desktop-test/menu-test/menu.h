62410d77a1439288c3927193e0249af6026e60a5
mjneri <michael.neri@eee.upd.edu.ph>
2022-04-14 15:50:06 +0800

    more minor changes to menu_object_t + removed unnecessary function pointers

--- desktop-test/menu-test/menu.h
+++ desktop-test/menu-test/menu.h
@@ -13,36 +13,32 @@
 // Maximum of 8 options per menu. Uses linked lists
 typedef struct menu_object_t
 {
+    char *self;
+    void (*selfHandler)(void);
+    struct menu_object_t *selfObj;
+
     char *option1;
-    void (*opt1Handler)(void);
     struct menu_object_t *option1Obj;
 
     char *option2;
-    void (*opt2Handler)(void);
     struct menu_object_t *option2Obj;
 
     char *option3;
-    void (*opt3Handler)(void);
     struct menu_object_t *option3Obj;
 
     char *option4;
-    void (*opt4Handler)(void);
     struct menu_object_t *option4Obj;
 
     char *option5;
-    void (*opt5Handler)(void);
     struct menu_object_t *option5Obj;
 
     char *option6;
-    void (*opt6Handler)(void);
     struct menu_object_t *option6Obj;
 
     char *option7;
-    void (*opt7Handler)(void);
     struct menu_object_t *option7Obj;
 
     char *option8;
-    void (*opt8Handler)(void);
     struct menu_object_t *option8Obj;
 
     struct menu_object_t *prevOption;
