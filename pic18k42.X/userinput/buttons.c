704468b3b75d9cc309c1333a721356d793d60bed
MJ <56290324+mjneri@users.noreply.github.com>
2022-12-11 14:46:40 +0800

    buttons.c - moved from graphics/gui to userinput/

---
+++ pic18k42.X/userinput/buttons.c
@@ -0,0 +1,195 @@
+//-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=
+// buttons.c -- Button debouncing
+//-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=
+// Author: mjneri
+//-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=
+//
+//-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=
+// Module Name: buttons.c
+// Description: Debounces button inputs through a state machine
+//
+// Revisions:
+// Revision 0.01 - File Created (2022-04-09)
+// Additional Comments:
+//
+//-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=
+
+// *****************************************************************************
+// *****************************************************************************
+// Section: Header Includes
+// *****************************************************************************
+// *****************************************************************************
+
+#include "buttons.h"
+
+// *****************************************************************************
+// *****************************************************************************
+// Section: Variables
+// *****************************************************************************
+// *****************************************************************************
+
+BUTTON_STATE buttonState;
+
+bool is_button_pressed;
+bool is_button_held;
+uint32_t button_press_count;         // how many button presses?
+uint8_t pressDebCount;               // used for debouncing button presses
+uint8_t unpressDebCount;             // for debouncing button unpresses
+
+// *****************************************************************************
+// *****************************************************************************
+// Section: Function Definitions
+// *****************************************************************************
+// *****************************************************************************
+
+bool get_button_pressed(void)
+{
+    return is_button_pressed;
+}
+
+bool get_button_held(void)
+{
+    return is_button_held;
+}
+
+uint32_t get_button_presscount(void)
+{
+    return button_press_count;
+}
+
+void reset_button_presscount(void)
+{
+    button_press_count = 0;
+}
+
+void Button_Initialize(void)
+{
+    buttonState = BTN_INIT;
+    
+    is_button_pressed = false;
+    is_button_held = false;
+    button_press_count = 0;
+    pressDebCount = 0;
+    unpressDebCount = 0;
+}
+
+void Button_Tasks(void)
+{
+#ifdef DEBUG_BTNTASKRATE
+    DEBUG_GPIO_OUT_Toggle();
+#endif
+    
+    switch(buttonState)
+    {
+        case BTN_INIT:
+        {
+            is_button_held = false;
+            is_button_pressed = false;
+            button_press_count = 0;
+            pressDebCount = 0;
+            unpressDebCount = 0;
+            
+            buttonState = BTN_WAIT_PRESS;
+            break;
+        }
+        
+        case BTN_WAIT_PRESS:
+        {
+            is_button_pressed = false;
+            is_button_held = false;
+            
+#ifdef DEBOUNCE_DEBUG
+            DEBUG_GPIO_OUT_SetHigh();
+#endif
+            
+            if(!SW0_GetValue())
+            {
+                buttonState = BTN_DEBOUNCE;
+            }
+            else
+            {
+                buttonState = BTN_WAIT_PRESS;
+            }
+            break;
+        }
+        
+        case BTN_DEBOUNCE:
+        {
+            if(!SW0_GetValue())
+            {
+                unpressDebCount = 0;    // Reset this since button is pressed
+                if(pressDebCount > DEBOUNCECOUNT)
+                {
+                    pressDebCount = 0;  // Reset
+                    buttonState = BTN_PRESSED;
+                }
+                else
+                {
+                    pressDebCount++;
+                    buttonState = BTN_DEBOUNCE;
+                }
+            }
+            else
+            {
+                pressDebCount = 0;      // Reset this since button is not pressed
+                if(unpressDebCount > DEBOUNCECOUNT)
+                {
+                    unpressDebCount = 0;
+                    buttonState = BTN_WAIT_PRESS;
+                }
+                else
+                {
+                    unpressDebCount++;
+                    buttonState = BTN_DEBOUNCE;
+                }
+            }
+            break;
+        }
+        
+        case BTN_PRESSED:
+        {
+            is_button_pressed = true;
+            button_press_count++;
+            
+#ifdef DEBOUNCE_DEBUG
+            DEBUG_GPIO_OUT_SetLow();
+#endif
+            
+            if(!SW0_GetValue())
+            {
+                buttonState = BTN_HELD;
+            }
+            else
+            {
+                buttonState = BTN_DEBOUNCE;
+            }
+            break;
+        }
+        
+        case BTN_HELD:
+        {
+            is_button_held = true;
+            is_button_pressed = true;
+            
+            if(!SW0_GetValue())
+            {
+                buttonState = BTN_HELD;
+            }
+            else
+            {
+                buttonState = BTN_DEBOUNCE;
+            }
+            break;
+        }
+        
+        default:
+        {
+            break;
+        }
+    }
+}
+
+/*******************************************************************************
+ End of File
+*/
+
