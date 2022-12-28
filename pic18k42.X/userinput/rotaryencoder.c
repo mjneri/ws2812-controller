349b8c79291843294669e3c5cdf3a92735a66335
MJ <56290324+mjneri@users.noreply.github.com>
2022-12-28 20:13:50 +0800

    rotary encoder module
    + Implemented a ring buffer; each entry is only 1-bit wide.
    + Ring buffer will store decoded rotary encoder inputs only
    + Implemented a function to get rotation count
    + Only the ring buffer read function is exposed via an API... the write function is used only by the CLC ISR.

--- pic18k42.X/userinput/rotaryencoder.c
+++ pic18k42.X/userinput/rotaryencoder.c
@@ -25,10 +25,111 @@
 
 // *****************************************************************************
 // *****************************************************************************
+// Section: Variables
+// *****************************************************************************
+// *****************************************************************************
+
+rot_ring_buf_t rotData;
+
+static volatile uint16_t rotCntCCW = 0;
+static volatile uint16_t rotCntCW = 0;
+
+// *****************************************************************************
+// *****************************************************************************
 // Section: Local Function Defines
 // *****************************************************************************
 // *****************************************************************************
 
+static int ROTENC_WriteRingBuf(ROT_DIR dir)
+{
+    if(rotData.isFull)
+    {
+        // Ring buffer is full. Do nothing
+        return -1;
+    }
+    else
+    {
+        // Write to the ring buffer
+        if(dir == ROTENC_CW)
+        {
+            rotData.buffer[rotData.wrIndex] |= 1 << rotData.wrBitIndex;
+        }
+        else
+        {
+            rotData.buffer[rotData.wrIndex] &= ~(1 << rotData.wrBitIndex);
+        }
+        
+        rotData.wrBitIndex = (rotData.wrBitIndex + 1) % 8;
+        
+        // if wrBitIndex has circled back to 0, increment the wrIndex.
+        if(rotData.wrBitIndex == 0)
+        {
+            rotData.wrIndex = (rotData.wrIndex + 1) % ROTENC_RINGBUF_MAXSIZE;
+        }
+        
+        // Since 1 byte has been written, the buffer is no longer empty
+        rotData.isEmpty = false;
+        rotData.dataAmt++;
+        
+        // Check if the buffer is now full after writing
+        if(rotData.dataAmt == ROTENC_RINGBUF_DATA_MAX)
+        {
+            rotData.isFull = true;
+        }
+    }
+    
+    return 0;
+}
+
+// *****************************************************************************
+// *****************************************************************************
+// Section: Callback Functions
+// *****************************************************************************
+// *****************************************************************************
+
+// TIMER CALLBACKS
+void RotClkDebounce(void)
+{
+    if(!RC6_GetValue())
+    {
+        DEB_ROT_A_SetLow();
+    }
+    else
+    {
+        DEB_ROT_A_SetHigh();
+    }
+}
+
+void RotDTDebounce(void)
+{
+    if(!RD0_GetValue())
+    {
+        DEB_ROT_B_SetLow();
+    }
+    else
+    {
+        DEB_ROT_B_SetHigh();
+    }
+}
+
+// CLC CALLBACKS
+void CLC2_Callback(void)
+{
+    if(CLC2_OutputStatusGet())
+    {
+        rotCntCCW++;
+        ROTENC_WriteRingBuf(ROTENC_CCW);
+    }
+}
+
+void CLC3_Callback(void)
+{
+    if(CLC3_OutputStatusGet())
+    {
+        rotCntCW++;
+        ROTENC_WriteRingBuf(ROTENC_CW);
+    }
+}
 
 // *****************************************************************************
 // *****************************************************************************
@@ -36,6 +137,70 @@
 // *****************************************************************************
 // *****************************************************************************
 
+void ROTENC_Initialize(void)
+{
+    memset(&rotData, 0, sizeof(rotData));
+    rotData.isEmpty = true;
+    
+    // Register callbacks for rotary encoder debouncing
+    // Note: TMR4 and TMR6 period is 1ms
+    TMR4_SetInterruptHandler(RotClkDebounce);
+    TMR6_SetInterruptHandler(RotDTDebounce);
+    
+    return;
+}
+
+ROT_DIR ROTENC_ReadRingBuf(void)
+{
+    ROT_DIR retData = ROTENC_ERR;
+    
+    if(rotData.isEmpty)
+    {
+        // Ring buffer is empty; do nothing
+    }
+    else
+    {
+        // Ring buffer contains some data; read some data
+        retData = (rotData.buffer[rotData.rdIndex] >> rotData.rdBitIndex) & 0x01;
+        
+        // Advance indices
+        rotData.rdBitIndex = (rotData.rdBitIndex + 1) % 8;
+        
+        // If rdBitIndex has circled back to 0, increment rdIndex
+        if(rotData.rdBitIndex == 0)
+        {
+            rotData.rdIndex = (rotData.rdIndex + 1) % ROTENC_RINGBUF_MAXSIZE;
+        }
+        
+        // Since 1 byte has been read, the buffer is no longer full
+        rotData.isFull = false;
+        rotData.dataAmt--;
+        
+        // Check if the buffer is now empty after reading
+        if(rotData.dataAmt == 0)
+        {
+            rotData.isEmpty = true;
+        }
+    }
+    
+    return retData;
+}
+
+void ROTENC_GetRotationCount(uint16_t *cw_count, uint16_t *ccw_count)
+{
+    if(cw_count == NULL)
+    {
+        return;
+    }
+    if(ccw_count == NULL)
+    {
+        return;
+    }
+    
+    *cw_count = rotCntCW;
+    *ccw_count = rotCntCCW;
+    return;
+}
 
 /*******************************************************************************
  End of File
--- pic18k42.X/userinput/rotaryencoder.h
+++ pic18k42.X/userinput/rotaryencoder.h
@@ -24,11 +24,16 @@
 // *****************************************************************************
 // *****************************************************************************
 
+#include "../mcc_generated_files/mcc.h"
+
 // *****************************************************************************
 // *****************************************************************************
 // Section: Constant Defines
 // *****************************************************************************
 // *****************************************************************************
+
+#define ROTENC_RINGBUF_MAXSIZE     8
+#define ROTENC_RINGBUF_DATA_MAX    (ROTENC_RINGBUF_MAXSIZE*8)
 
 // *****************************************************************************
 // *****************************************************************************
@@ -42,14 +47,44 @@
 // *****************************************************************************
 // *****************************************************************************
 
+// Rotation direction
+typedef enum
+{
+    ROTENC_CCW = 0,
+    ROTENC_CW,
+            
+    ROTENC_ERR = 0xFF
+} ROT_DIR;
+
+// Ring Buffer
+typedef struct
+{
+    volatile uint8_t buffer[ROTENC_RINGBUF_MAXSIZE];
+    uint8_t rdIndex;            // Index pointing to data to be read
+    uint8_t rdBitIndex;         // Bit index within the byte...
+
+    uint8_t wrIndex;            // Index pointing to data to be written
+    uint8_t wrBitIndex;         // Bit index within the byte...
+
+    bool isEmpty;               // Flag; set when the ring buffer is empty
+    bool isFull;                // Flag; set when the ring buffer is full
+    uint8_t dataAmt;            // Determines how many ring buffer elements are stored
+} rot_ring_buf_t;
+
 // *****************************************************************************
 // *****************************************************************************
 // Section: Function Prototypes
 // *****************************************************************************
 // *****************************************************************************
 
+void CLC2_Callback(void);
+void CLC3_Callback(void);
 
+void ROTENC_Initialize(void);
 
+ROT_DIR ROTENC_ReadRingBuf(void);
+
+void ROTENC_GetRotationCount(uint16_t *cw_count, uint16_t *ccw_count);
 
 #endif /*_ROTARYENCODER_H_*/
 
