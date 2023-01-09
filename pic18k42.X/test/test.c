689746859079d57b4be3b4b8e05243f652b6fb05
MJ <56290324+mjneri@users.noreply.github.com>
2023-01-09 20:55:34 +0800

    test.c - code cleanup and velocity testing
    + Removed old unused code
    + Cleaned up test code to reduce redundant lines
    + Added some test code for testing velocity - the idea is that a counter value's rate of change depends on how fast the encoder is turned.
    
    >NOTE: the velocity test code will still need some tweaking to ensure it works properly.

--- pic18k42.X/test/test.c
+++ pic18k42.X/test/test.c
@@ -8,10 +8,17 @@
 #define LEDLATCH() __delay_ms(1)        // wait for LED to latch
 
 // Globals for testing
-volatile uint8_t selected = 0;
-volatile bool switchSelected = false;
-volatile bool isSwitchPressed = false;
-volatile bool isSwitchHeld = false;
+uint64_t t0 = 0;
+char teststring[64];
+uint16_t cw_count, ccw_count;
+ROT_DIR rotDirection;
+const char *dir_ascii[] = {"CCW", "CW"};
+
+uint16_t rotEnc_InputCounter = 0;       // Counts buffered inputs
+uint16_t buf_cw_count = 0, buf_ccw_count = 0;   // Increments based on buffered inputs
+
+int64_t testcounter = 0;        // for testing with velocity function
+uint16_t rotVelocity = 0;
 
 // Callback for heartbeat LED
 void HeartbeatCallback(void)
@@ -19,30 +26,16 @@ void HeartbeatCallback(void)
     LED0_Toggle();
 }
 
-void DebounceCallback(void)
-{
-    // NOTE: Make sure SW0 (RE2) is connected to TM4_ers input (RC6)
-    // NOTE: Make sure this callback function is registered
-    if(!SW0_GetValue())
-    {
-        isSwitchPressed = true;
-    }
-    else
-    {
-        isSwitchPressed = false;
-    }
-}
-
 // ************************************************************
 
 // Local function prototypes
 static void TEST_PIXELS(void);
+static void TEST_Display(void);
 
 // Other local function prototypes
-void displayMenu(uint8_t selectedOption);
 static void profitestCometsTail(uint8_t tailLen);
 
-// TEST CODE FOR USER INPUTS ********************************************
+// TEST CODE FOR USER INPUTS
 static void TEST_USERINPUTS(void);
 
 // Velocity testing
@@ -56,226 +49,156 @@ void TEST_Function(void)
     // Register the heartbeat callback function
     TMR1_SetInterruptHandler(HeartbeatCallback);
     
-    // Register TMR4 Callback
-    //TMR4_SetInterruptHandler(DebounceCallback);
+    // Initialize millis and some variables
+    millis_Initialize();
+    Button_Initialize();
+    ROTENC_Initialize();
+    
+    // Make sure to clear the software framebuffer after initializing the OLED
+    __delay_ms(100);
+    OLED_Initialize();
+    GFX_Clear();
     
     // Test SPI LED functions
     //TEST_PIXELS();
     
-    // Test the button debouncing state machine code
-    //TEST_USERINPUTS();
+    // Test code for anything involving the OLED in some way
+    TEST_Display();
+    return;
+}
+
+static void TEST_Display(void)
+{
+    // To render the bitmap, call OLED_Tasks() until all pages have been updated.
+    // Here, we assume that this process is finished after 1sec.
+    // We should expect that the ring buffer is empty once all pages have been updated.
+    GFX_Draw(nailandgear);
+    GFX_Render();
+    t0 = millis();
     
-    // Test velocity
-    TEST_VELOCITY();
+    // Keep calling OLED_Tasks until 1s has passed.
+    while(millis() - t0 < 1000)
+    {
+        OLED_Tasks();
+    }
+    
+    // Clear fbuf again so text has a blank canvas to work with.
+    GFX_Clear();
+    
+    // Re-anchor t0
+    t0 = millis();
+    
+    while(true)
+    {
+        Button_Tasks();
+        OLED_Tasks();
+        
+        // Read buffered inputs
+        rotDirection = ROTENC_ReadRingBuf();
+        if(rotDirection != ROTENC_ERR)
+        {
+            // Increment the buffered input counter.
+            rotEnc_InputCounter++;
+
+            if(rotDirection == ROTENC_CW)
+            {
+                buf_cw_count++;
+                testcounter = testcounter + rotVelocity;
+            }
+            else
+            {
+                buf_ccw_count++;
+                testcounter = testcounter - rotVelocity;
+            }
+        }
+        
+        // Update the screen whenever possible (i.e. fastest refresh rate)
+        if(!OLED_IsBusy())
+        {
+            //TEST_USERINPUTS();
+            TEST_VELOCITY();
+            
+            GFX_Render();
+            t0 = millis();
+        }
+    }
+}
+
+
+static void TEST_USERINPUTS(void)
+{   
+    // Count button presses
+    sprintf(teststring, "SW Press: %lu", get_button_presscount());
+    GFX_Text(0, 0, teststring, &font5x7, 0);
+
+    // Show whether button is press and held
+    sprintf(teststring, "Status P: %d H: %d", get_button_pressed(), get_button_held());
+    GFX_Text(8, 0, teststring, &font5x7, 0);
+
+    // Display **ACTUAL** rotation count
+    ROTENC_GetRotationCount(&cw_count, &ccw_count);
+    sprintf(teststring, "CCW: %d CW: %d = %d", ccw_count, cw_count, ccw_count+cw_count);
+    GFX_Text(16, 0, teststring, &font5x7, 0);
+
+    // Display buffered rotation direction.
+    if(rotDirection != ROTENC_ERR)
+    {
+        sprintf(teststring, "Rotation: %s   ", dir_ascii[rotDirection]);
+        GFX_Text(32, 0, teststring, &font5x7, 0);
+    }
+    else
+    {
+        sprintf(teststring, "Rotation: ---------");
+        GFX_Text(32, 0, teststring, &font5x7, 0);
+    }
+
+    // Display total buffered inputs
+    sprintf(teststring, "Buffered: %u", rotEnc_InputCounter);
+    GFX_Text(40, 0, teststring, &font5x7, 0);
+
+    // Display buffered CW and CCW inputs
+    sprintf(teststring, "BUF CCW: %u CW: %u", buf_ccw_count, buf_cw_count);
+    GFX_Text(48, 0, teststring, &font5x7, 0);
+
+    // Display millis() in realtime
+    sprintf(teststring, "millis = %llu", millis());
+    GFX_Text(56, 0, teststring, &font5x7, 0);
     
     return;
 }
 
-// TEST CODE FOR BUTTON DEBOUNCING SM ********************************************
-static void TEST_USERINPUTS(void)
-{
-    // Initialize millis and some variables
-    millis_Initialize();
-    Button_Initialize();
-    ROTENC_Initialize();
-    
-    // Variables
-    uint64_t t0 = 0;
-    char teststring[64];
-    uint16_t cw_count, ccw_count;
-    ROT_DIR rotDirection;
-    const char *dir_ascii[] = {"CCW", "CW"};
-    
-    uint16_t rotEnc_InputCounter = 0;       // Counts buffered inputs
-    uint16_t buf_cw_count = 0, buf_ccw_count = 0;   // Increments based on buffered inputs
-    
-    // Make sure to clear the software framebuffer after initializing the OLED
-    __delay_ms(100);
-    OLED_Initialize();
-    GFX_Clear();
-    
-    // To render the bitmap, call OLED_Tasks() until all pages have been updated.
-    // Here, we assume that this process is finished after 1sec.
-    // We should expect that the ring buffer is empty once all pages have been updated.
-    GFX_Draw(nailandgear);
-    GFX_Render();
-    t0 = millis();
-    
-    // Keep calling OLED_Tasks until 1s has passed.
-    while(millis() - t0 < 1000)
-    {
-        OLED_Tasks();
-    }
-    
-    // Clear fbuf again so text has a blank canvas to work with.
-    GFX_Clear();
-    
-    // Re-anchor t0
-    t0 = millis();
-    while(1)
-    {
-        Button_Tasks();
-        OLED_Tasks();
-        
-        // Read buffered inputs
-        rotDirection = ROTENC_ReadRingBuf();
-        if(rotDirection != ROTENC_ERR)
-        {
-            // Increment the buffered input counter.
-            rotEnc_InputCounter++;
-
-            if(rotDirection == ROTENC_CW)
-            {
-                buf_cw_count++;
-            }
-            else
-            {
-                buf_ccw_count++;
-            }
-        }
-        
-        // Update the screen whenever possible (i.e. fastest refresh rate)
-        if(!OLED_IsBusy())
-        {
-            // Count button presses
-            sprintf(teststring, "SW Press: %lu", get_button_presscount());
-            GFX_Text(0, 0, teststring, &font5x7, 0);
-            
-            // Show whether button is press and held
-            sprintf(teststring, "Status P: %d H: %d", get_button_pressed(), get_button_held());
-            GFX_Text(8, 0, teststring, &font5x7, 0);
-            
-            // Display **ACTUAL** rotation count
-            ROTENC_GetRotationCount(&cw_count, &ccw_count);
-            sprintf(teststring, "CCW: %d CW: %d = %d", ccw_count, cw_count, ccw_count+cw_count);
-            GFX_Text(16, 0, teststring, &font5x7, 0);
-            
-            // Display buffered rotation direction.
-            if(rotDirection != ROTENC_ERR)
-            {
-                sprintf(teststring, "Rotation: %s   ", dir_ascii[rotDirection]);
-                GFX_Text(32, 0, teststring, &font5x7, 0);
-            }
-            else
-            {
-                sprintf(teststring, "Rotation: ---------");
-                GFX_Text(32, 0, teststring, &font5x7, 0);
-            }
-            
-            // Display total buffered inputs
-            sprintf(teststring, "Buffered: %u", rotEnc_InputCounter);
-            GFX_Text(40, 0, teststring, &font5x7, 0);
-            
-            // Display buffered CW and CCW inputs
-            sprintf(teststring, "BUF CCW: %u CW: %u", buf_ccw_count, buf_cw_count);
-            GFX_Text(48, 0, teststring, &font5x7, 0);
-            
-            // Display millis() in realtime
-            sprintf(teststring, "millis = %llu", millis());
-            GFX_Text(56, 0, teststring, &font5x7, 0);
-            
-            GFX_Render();
-            
-            t0 = millis();
-        }
-    }
-}
-
 static void TEST_VELOCITY(void)
 {
-    // Initialize millis and some variables
-    millis_Initialize();
-    Button_Initialize();
-    ROTENC_Initialize();
-    
-    // Variables
-    uint64_t t0 = 0;
-    char teststring[64];
-    uint16_t cw_count, ccw_count;
-    ROT_DIR rotDirection;
-    const char *dir_ascii[] = {"CCW", "CW"};
-    
-    uint16_t rotEnc_InputCounter = 0;       // Counts buffered inputs
-    uint16_t buf_cw_count = 0, buf_ccw_count = 0;   // Increments based on buffered inputs
-    
-    // Make sure to clear the software framebuffer after initializing the OLED
-    __delay_ms(100);
-    OLED_Initialize();
-    GFX_Clear();
-    
-    // To render the bitmap, call OLED_Tasks() until all pages have been updated.
-    // Here, we assume that this process is finished after 1sec.
-    // We should expect that the ring buffer is empty once all pages have been updated.
-    GFX_Draw(nailandgear);
-    GFX_Render();
-    t0 = millis();
-    
-    // Keep calling OLED_Tasks until 1s has passed.
-    while(millis() - t0 < 1000)
-    {
-        OLED_Tasks();
-    }
-    
-    // Clear fbuf again so text has a blank canvas to work with.
-    GFX_Clear();
-    
-    // Re-anchor t0
-    t0 = millis();
-    
-    while(1)
-    {
-        Button_Tasks();
-        OLED_Tasks();
-        
-        // Read buffered inputs
-        rotDirection = ROTENC_ReadRingBuf();
-        if(rotDirection != ROTENC_ERR)
-        {
-            // Increment the buffered input counter.
-            rotEnc_InputCounter++;
+    // Count button presses
+    sprintf(teststring, "SW Press: %lu", get_button_presscount());
+    GFX_Text(0, 0, teststring, &font5x7, 0);
 
-            if(rotDirection == ROTENC_CW)
-            {
-                buf_cw_count++;
-            }
-            else
-            {
-                buf_ccw_count++;
-            }
-        }
-        
-        // Update the screen whenever possible (i.e. fastest refresh rate)
-        if(!OLED_IsBusy())
-        {
-            // Count button presses
-            sprintf(teststring, "SW Press: %lu", get_button_presscount());
-            GFX_Text(0, 0, teststring, &font5x7, 0);
-            
-            // Show whether button is press and held
-            sprintf(teststring, "Status P: %d H: %d", get_button_pressed(), get_button_held());
-            GFX_Text(8, 0, teststring, &font5x7, 0);
-            
-            // Print velocity value
-            sprintf(teststring, "Vel: %u t/s", ROTENC_Velocity());
-            GFX_Text(16, 0, teststring, &font5x7, 0);
-            
-            // Display total buffered inputs
-            sprintf(teststring, "Buffered: %u", rotEnc_InputCounter);
-            GFX_Text(40, 0, teststring, &font5x7, 0);
-            
-            // Display buffered CW and CCW inputs
-            sprintf(teststring, "BUF CCW: %u CW: %u", buf_ccw_count, buf_cw_count);
-            GFX_Text(48, 0, teststring, &font5x7, 0);
-            
-            // Display millis() in realtime
-            sprintf(teststring, "millis = %llu", millis());
-            GFX_Text(56, 0, teststring, &font5x7, 0);
-            
-            GFX_Render();
-            
-            t0 = millis();
-        }
-    }
+    // Show whether button is press and held
+    sprintf(teststring, "Status P: %d H: %d", get_button_pressed(), get_button_held());
+    GFX_Text(8, 0, teststring, &font5x7, 0);
+
+    // Print velocity value
+    rotVelocity = ROTENC_Velocity();
+    sprintf(teststring, "Vel: %u t/s", rotVelocity);
+    GFX_Text(16, 0, teststring, &font5x7, 0);
+    
+    // print testcounter value
+    sprintf(teststring, "%lld", testcounter);
+    GFX_Text(24, 0, teststring, &font5x7, 0);
+
+    // Display total buffered inputs
+    sprintf(teststring, "Buffered: %u", rotEnc_InputCounter);
+    GFX_Text(40, 0, teststring, &font5x7, 0);
+
+    // Display buffered CW and CCW inputs
+    sprintf(teststring, "BUF CCW: %u CW: %u", buf_ccw_count, buf_cw_count);
+    GFX_Text(48, 0, teststring, &font5x7, 0);
+
+    // Display millis() in realtime
+    sprintf(teststring, "millis = %llu", millis());
+    GFX_Text(56, 0, teststring, &font5x7, 0);
+    
+    return;
 }
 
 // ************************************************************
@@ -295,15 +218,6 @@ static void TEST_PIXELS(void)
         
     }
     return;
-}
-
-// Other local test functions
-void displayMenu(uint8_t selectedOption)
-{
-//    OLED_DrawString(0, 0, "Main Menu", font5x7, 0);
-//    OLED_DrawString(8, 5, "Select LED Profile", font5x7, (selectedOption == 0));
-//    OLED_DrawString(16, 5, "Adjust LED Count", font5x7, (selectedOption == 1));
-//    OLED_DrawString(24, 5, "About", font5x7, (selectedOption == 2));
 }
 
 static void profitestCometsTail(uint8_t tailLen)
